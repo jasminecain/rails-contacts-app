aec775bb7a0237ba1c6a55b90e03410a88e649dc
Jasmine Quach <xjasminequach@gmail.com>
2018-01-25 20:51:09 -0600

    Add destroy and edit functionaltiy

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,11 +13,38 @@ class ContactsController < ApplicationController
   end
 
   def new
+    @contact = Contact.new
+  end
+
+  def edit
+    @contact = Contact..find(params[:id])
   end
 
   def create
     @contact = Contact.new(params.require(:contact).permit(:name, :phone))
     @contact.save
-    redirect_to @contact
+
+    if @contact.save
+      redirect_to @contact
+    else
+      flash[:error]
+      render :new
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(params.require(:contact).permit(:name, :phone))
+      redirect_to @contact
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to :action => 'index'
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
-class Contact < ApplicationRecord
+class Contact < ActiveRecord::Base
+  validates :name, format: { with: /\A[A-Za-z ]+\z/ }
+  validates :phone, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
 end
